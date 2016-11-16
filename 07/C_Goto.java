d17e07c566aa3f25f7193dcaffb760e5df3e823f
hadasgross <hadas.gross1@mail.huji.ac.il>
2016-11-16 12:32:10 +0200

    Add files via upload
    changing files names

---
+++ 07/C_Goto.java
@@ -0,0 +1,7 @@
+package ex8;
+
+/**
+ * Created by hadas on 16/11/2016.
+ */
+public class C_Goto {
+}
---
+++ 07/C_If.java
@@ -0,0 +1,18 @@
+package ex8;
+
+import java.io.PrintWriter;
+
+/**
+ * Created by hadas on 16/11/2016.
+ */
+public class C_If extends VMCommand {
+    public String getFirstArg() {
+        return "";
+    }
+    public int getSecondArg() {
+        return 0;
+    }
+    public void translateToAsm(PrintWriter writer, Memory memory) {
+
+    }
+}
