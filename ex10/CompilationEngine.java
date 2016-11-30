5f49bfa8b7a28bd5045907d1af02faa763f46cf0
hadasgross <hadas.gross1@mail.huji.ac.il>
2016-11-30 09:05:11 +0200

    add parser template

---
+++ ex10/CompilationEngine.java
@@ -0,0 +1,55 @@
+package ex10;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+/**
+ * Created by hadas on 29/11/2016.
+ */
+public class CompilationEngine extends XmlWriter{
+    public CompilationEngine(String filename) throws FileNotFoundException{
+        super(filename);
+    }
+    public void compileClass() {
+
+    }
+    public void compileClassVarDec(){
+
+    }
+    public void compileSubroutine(){
+
+    }
+    public void compileParameterList(){
+
+    }
+    public void compileVarDec() {
+
+    }
+    public void compileStatements() {
+
+    }
+    public void compileDo() {
+
+    }
+    public void compileLet(){
+
+    }
+    public void compileWhile(){
+
+    }
+    public void compileReturn(){
+
+    }
+    public void compileIf() {
+
+    }
+    public void compileExpression(){
+
+    }
+    public void compileTerm(){
+
+    }
+    public void compileExpressionList(){
+
+    }
+}
